3e4cd0d43d401b9f92d4ec7293b6c177217e4e7a
melcam <yumelcam.93@gmail.com>
2022-01-23 23:50:31 -0500

    config server

---
+++ src/test/java/com/bootcamp/configServer/ConfigServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bootcamp.configServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@EnableConfigServer
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
